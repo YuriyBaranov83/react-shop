7aaa0eed6bd00f59060064df6c4a870cec6a85ba
YuriyBaranov83 <yuri.baranov83@gmail.com>
2026-03-01 21:24:10 +0200

    feat(router): add react-router-dom routes and create 404 page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,14 @@
-import { HomePage } from "@/pages";
+import { HomePage, NotFoundPage } from "@/pages";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/404" element={<NotFoundPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/not-found/NotFoundPage.jsx
@@ -0,0 +1,34 @@
+﻿import notFoundImage from "@/assets/images/404/404.webp";
+import { Link } from "react-router-dom";
+import styles from "./NotFoundPage.module.css";
+
+const NotFoundPage = () => {
+  return (
+    <main className={styles.notFoundPage}>
+      <section className={styles.notFoundCard} aria-labelledby="not-found-title">
+        <p className={styles.notFoundLabel}>помилка</p>
+
+        <div className={styles.notFoundCode} id="not-found-title">
+          <span>4</span>
+
+          <div className={styles.notFoundImageWrap} aria-hidden="true">
+            <img src={notFoundImage} alt="" loading="lazy" decoding="async" />
+          </div>
+
+          <span>4</span>
+        </div>
+
+        <p className={styles.notFoundDescription}>
+          Ой! Здається, щось пішло не так. Сторінка, яку ви запитуєте, не існує або
+          адресу було введено неправильно.
+        </p>
+
+        <Link to="/" className={styles.notFoundHomeLink}>
+          Перейти на головну
+        </Link>
+      </section>
+    </main>
+  );
+};
+
+export default NotFoundPage;
